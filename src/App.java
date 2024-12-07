3ddf4e81d4de8c8ec9f6c3b7e92830edcba8def1
SanathVenkatachalam <venkatachalam0362@mydusd.org>
2024-12-06 21:23:39 -0800

    jsoup is working :)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        try {
+            Document doc = Jsoup.connect("https://google.com").get();
+            System.out.println(doc.title());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
