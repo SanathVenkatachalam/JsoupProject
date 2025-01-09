746330e0c532d8ab90b0c223e2ff1814f16439c9
SanathVenkatachalam <venkatachalam0362@mydusd.org>
2025-01-09 14:00:39 -0800

    Filtered out Dog reviews

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Locale;
@@ -22,20 +23,31 @@ public class App {
         // Try to extract the reviews (assuming they are in div elements with a specific class)
         // You will need to inspect the HTML and find the right CSS selector
 
-        String [] keywordsDog = {"puppy" , "barking", "stray", "cute", "little", "doggie","pup","chewed","spot"};
-        FileWriter dogWriter = new FileWriter("DogFood.txt");
-        File file = new File("C:\\Users\\5\\Documents\\Github\\JsoupProject\\src\\socialMediaPosts.txt");
-        Scanner reviews = new Scanner(file);
+        String [] keywordsDog = {"puppy" , "stray", "little", "doggie","pup","chewed","spot"};
+        //FileWriter dogWriter = new FileWriter("DogFood.txt");
+        File file = new File("C:\\Users\\5\\Documents\\Github\\JsoupProject\\src\\DogFood.txt");
+        FileWriter dog = new FileWriter(file);
+        File fileReview = new File("C:\\Users\\5\\Documents\\Github\\JsoupProject\\src\\socialMediaPosts.txt");
+        Scanner reviews = new Scanner(fileReview);
         reviews.useLocale(Locale.US);
         //String review = reviews.nextLine();
         //System.out.println(reviewOne);
+        //dog.write("Hello");
+
 
 
 
 
         while(reviews.hasNextLine()){
-            
-            System.out.println(reviews.nextLine());
+            String reviewToCheck = reviews.nextLine();
+            for (String keyword : keywordsDog) {
+                if (reviewToCheck.contains(keyword)) {
+                    System.out.println(reviewToCheck+ " This review has dogs");
+                    break;
+                }
+            }
+    
+
         }
 
         
