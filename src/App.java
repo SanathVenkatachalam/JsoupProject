6e535035d4612613c4edbd1cb3df269a10182408
SanathVenkatachalam <venkatachalam0362@mydusd.org>
2025-01-13 13:23:41 -0800

    Add the advertisements and username listing

--- src/App.java
+++ src/App.java
@@ -13,22 +13,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws IOException{
-        // Connect to the website and fetch the HTML document
         //Document doc = Jsoup.connect("https://www.trustpilot.com/review/jbl.com").get();
         
-        // Print the title of the page
-        //System.out.println("Page Title: " + doc.title());
-        
-        // Try to extract the reviews (assuming they are in div elements with a specific class)
-        // You will need to inspect the HTML and find the right CSS selector
 
 
-        //we used canned data.
+        //we used canned data.(SocialMediaPosts.txt)
+
+        //assign keywords to each category of pet ( for this we are using dogs and cats)
         String [] keywordsDog = {"puppy" , "stray", "little", "doggie","pup","chewed","spot","Dogs","fur","wolf","treats"};
         String [] keywordCat = {"kitten","kitty","cat"};
+        //initialize the file names
         String dogFood = "src/DogFood.txt";
         String review = "src/socialMediaPosts.txt";
         String catFood = "src/CatFood.txt";
+        //create files and writers for that file(this is for the adding process)
         File file = new File(dogFood);
         FileWriter dog = new FileWriter(file,true);
         File fileTwo = new File(catFood);
@@ -36,22 +34,25 @@ public class App {
         File fileReview = new File(review);
         Scanner reviews = new Scanner(fileReview);
         reviews.useLocale(Locale.US);
+        //initialize the username lists for advertisements
         ArrayList<String> dogUsernames = new ArrayList<>();
         ArrayList<String> catUsernames = new ArrayList<>();
+        //loop through the data and see if the reviews have the keywords in them.
         while(reviews.hasNextLine()){
             String reviewToCheck = reviews.nextLine();
             for (String keyword : keywordsDog) {
                 if (reviewToCheck.contains(keyword)) {
                     String[] splitedReview = reviewToCheck.split(" ");
+                    //single out the username and grab it.
                     dogUsernames.add(splitedReview[0]);
                     dog.write(reviewToCheck + "\n");
-                    
                     break;
                 }
             }
             for (String keyword : keywordCat) {
                 if (reviewToCheck.contains(keyword)) {
                     String[] splitedReview = reviewToCheck.split(" ");
+                    //single out the username and grab it.
                     catUsernames.add(splitedReview[0]);
                     cat.write(reviewToCheck + "\n");
                     break;
@@ -60,8 +61,28 @@ public class App {
         }
         dog.close();
         cat.close();
-        System.out.println(dogUsernames);
-        System.out.println(catUsernames);
+        //Display the advertisements
+        //Dog owner ads.
+        System.out.println("Dog Owners");
+        System.out.println();
+        for(String username : dogUsernames){
+            //advertisement will appeal to EACH individual username on the review.
+            System.out.println( username + "Buy out premuim canned dog food now. Taste that will attract a house full of them. So healthy that they will grow as big as a wolf. Comes with a raffle to win a free doggie hotel!");
+            System.out.println();
+        }
+        //Cat owner ads.
+        System.out.println();
+        System.out.println("Cat Owners");
+        System.out.println();
+
+
+        for(String username : catUsernames){
+            //advertisement will appeal to EACH individual username on the review.
+            System.out.println( username + "Buy our rich and premium at food today! This food is so tastey that your little kitten friends will stop tearing up your shoes!");
+            System.out.println();
+        }
+
+
 
 
         
